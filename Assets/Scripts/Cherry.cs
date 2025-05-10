07e919cd712e379265b9cf6be1a47c2f0a93d9c7
Rick Rietbergen <rbps3.2016@gmail.com>
2025-05-10 23:54:29 +0200

    Add cherry to grid + add points to score

---
+++ Assets/Scripts/Cherry.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Cherry : MonoBehaviour, IEatable
+    {
+        [SerializeField]
+        private int score = 100;
+
+        public void Eaten()
+        {
+            gameObject.SetActive(false);
+            ScoreManager.Instance.AddPoints(score);
+        }
+    }
+}

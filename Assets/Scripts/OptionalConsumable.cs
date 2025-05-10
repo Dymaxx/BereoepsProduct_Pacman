5ab37cd73c6544342ae2d5e986958fc9661097a8
Wiemarp <wiemar.pomme@icloud.com>
2025-05-10 22:51:11 +0200

    Added optional consumable class

---
+++ Assets/Scripts/OptionalConsumable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class OptionalConsumable : MonoBehaviour, IEatable
+    {
+        [SerializeField]
+        private int score = 100;
+
+        public void Eaten()
+        {
+            ScoreManager.Instance.AddPoints(score);
+        }
+    }
+}

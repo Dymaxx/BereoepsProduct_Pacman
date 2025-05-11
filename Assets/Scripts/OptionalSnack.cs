7f5d23cdd1bce8966719b350e37be574ca99911a
Rick Rietbergen <rbps3.2016@gmail.com>
2025-05-11 16:20:35 +0200

    snack toegevoegd

---
+++ Assets/Scripts/OptionalSnack.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class OptionalSnack : MonoBehaviour, IEatable
+    {
+        [SerializeField]
+        private int score;
+
+        [SerializeField]
+        private Vector3 spawnPosition;
+
+        [SerializeField]
+        private float spawnInterval;
+
+        private void Awake()
+        {
+            // Zet SpriteRenderer & collider uit
+            if (gameObject != null)
+            {
+                if (gameObject.TryGetComponent<SpriteRenderer>(out var prefabRenderer))
+                {
+                    prefabRenderer.enabled = false;
+                }
+
+                if (gameObject.TryGetComponent<Collider2D>(out var prefabCollider))
+                {
+                    prefabCollider.enabled = false;
+                }
+
+                StartCoroutine(SpawnSnackAfterDelay());
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Eaten()
+        {
+            gameObject.SetActive(false);
+            ScoreManager.Instance.AddPoints(score);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator SpawnSnackAfterDelay()
+        {
+            yield return new WaitForSeconds(spawnInterval);
+
+            GameObject newSnack = Instantiate(gameObject, spawnPosition, Quaternion.identity);
+
+            // SpriteRenderer aanzetten
+            if (newSnack.TryGetComponent<SpriteRenderer>(out var newRenderer))
+            {
+                newRenderer.enabled = true;
+            }
+
+            // Collider weer activeren
+            if (newSnack.TryGetComponent<Collider2D>(out var newCollider))
+            {
+                newCollider.enabled = true;
+            }
+        }
+    }
+}
+ 
--- Assets/Scripts/SnackSpawner.cs
+++ Assets/Scripts/SnackSpawner.cs
@@ -1,55 +1,20 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Assets.Scripts
 {
     public class SnackSpawner : MonoBehaviour
     {
         [SerializeField]
-        private GameObject objectPrefab;
-
-        [SerializeField]
-        private Vector3 spawnPosition;
-
-        [SerializeField]
-        private float spawnDelay;
+        private OptionalSnack[] snackArray;
 
         private void Awake()
         {
-            // Zet SpriteRenderer op prefab tijdelijk uit (alleen renderen uitschakelen)
-            if (objectPrefab != null)
+            foreach (var snack in snackArray)
             {
-                if (objectPrefab.TryGetComponent<SpriteRenderer>(out var prefabRenderer))
+                if (snack != null)
                 {
-                    prefabRenderer.enabled = false;
+                    StartCoroutine(snack.SpawnSnackAfterDelay());
                 }
-
-                if (objectPrefab.TryGetComponent<Collider2D>(out var prefabCollider))
-                {
-                    prefabCollider.enabled = false;
-                }
-
-                StartCoroutine(SpawnSnackAfterDelay());
-            }
-        }
-
-        private IEnumerator SpawnSnackAfterDelay()
-        {
-            yield return new WaitForSeconds(spawnDelay);
-
-            GameObject newSnack = Instantiate(objectPrefab, spawnPosition, Quaternion.identity);
-
-            // SpriteRenderer aanzetten
-            SpriteRenderer newRenderer = newSnack.GetComponent<SpriteRenderer>();
-            if (newRenderer != null)
-            {
-                newRenderer.enabled = true;
-            }
-
-            // Collider weer activeren
-            if (newSnack.TryGetComponent<Collider2D>(out var newCollider))
-            {
-                newCollider.enabled = true;
             }
         }
     }
